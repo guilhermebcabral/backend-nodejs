2e513c30fcbe02434dacefca8e3ea621cf67b08d
guilhermebcabral <guibcabral19@gmail.com>
2020-12-15 21:32:50 -0300

    users migration and model

--- src/database/migrations/1608077821453-CreateUsers.ts
+++ src/database/migrations/1608077821453-CreateUsers.ts
@@ -1,12 +1,10 @@
-import { query } from "express";
 import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
-export default class CreateAppointments1607556209634 implements MigrationInterface {
+export default class CreateUsers1608077821453 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
-      // Criando tabelas
       new Table({
-        name: 'appointments',
+        name: 'users',
         columns: [
           {
             name: 'id',
@@ -16,12 +14,17 @@ export default class CreateAppointments1607556209634 implements MigrationInterfa
             default: 'uuid_generate_v4()',
           },
           {
-            name: 'provider',
+            name: 'name',
             type: 'varchar',
           },
           {
-            name: 'date',
-            type: 'timestamp with time zone',
+            name: 'email',
+            type: 'varchar',
+            isUnique: true
+          },
+          {
+            name: 'password',
+            type: 'varchar',
           },
           {
             name: 'created_at',
@@ -39,6 +42,7 @@ export default class CreateAppointments1607556209634 implements MigrationInterfa
   }
   // fallback -> deletar/desfazer o que foi feito no método up
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('appointments')
+    await queryRunner.dropTable('users')
   }
+
 }
