f6195e2595fb2ab6e1ddb69edb32be552f55ef45
guilhermebcabral <guibcabral19@gmail.com>
2020-12-23 11:34:01 -0300

    add jwt

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 import User from '../models/User';
 
 interface Request {
@@ -9,6 +10,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -31,9 +33,13 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination');
     }
 
-    // Usuário autenticado
+    const token = sign({}, 'c6da1619d976e818e6a6178a813c26c7', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
     return {
       user,
+      token,
     };
   }
 }
