eedbc54c1bf113d42d5300609651eb5ae630c5a4
guilhermebcabral <guibcabral19@gmail.com>
2020-12-27 12:30:05 -0300

    global exception handler

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,6 +1,7 @@
 import { getRepository } from 'typeorm';
 import { hash } from 'bcryptjs';
 import User from '../models/User';
+import AppError from '../errors/AppError';
 
 interface Request {
   name: string;
@@ -17,7 +18,7 @@ class CreateUserService {
     });
 
     if (checkuserExists) {
-      throw new Error('Email address already');
+      throw new AppError('Email address already');
     }
 
     const hashedPassword = await hash(password, 8);
